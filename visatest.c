21d48c4780ceb4abd325689c4b1bccabee7c51ff
Magnus Danielson <magnus@rubidium.se>
2024-05-29 15:15:42 +0200

    Added shared lock count test on unlock

--- visatest.c
+++ visatest.c
@@ -250,8 +250,14 @@ int main()
 	TB_TEST_EXPECT_M_LINT(visa, retval, VI_SUCCESS, "VISA Rule 3.6.9");
 	TB_TEST_EXPECT_M_UINT(visa, shrd_count1, shrd_count0+1, "VISA 3.6.10");
 	// Rule 3.6.33
+	// Rule 3.6.35
+	retval = viGetAttribute(vi,VI_ATTR_RSRC_SHRD_LOCK_COUNT,&shrd_count0);
+	TB_TEST_EXPECT_M_LINT(visa, retval, VI_SUCCESS, "VISA Rule 3.6.9");
 	retval = viUnlock(vi);
 	TB_TEST_EXPECT_M_LINT(visa, retval, VI_SUCCESS_NESTED_SHARED, "VISA 3.6.33 VI_SUCCESS_NESTED_SHARED");
+	retval = viGetAttribute(vi,VI_ATTR_RSRC_SHRD_LOCK_COUNT,&shrd_count1);
+	TB_TEST_EXPECT_M_LINT(visa, retval, VI_SUCCESS, "VISA Rule 3.6.9");
+	TB_TEST_EXPECT_M_UINT(visa, shrd_count1, shrd_count0-1, "VISA 3.6.35");
 
 	free(access_key);
 	free(requested_key);
@@ -295,7 +301,6 @@ int main()
 	// Rule 3.6.29
 	// Rule 3.6.31
 
-	// Rule 3.6.35
 	// Rule 3.6.36
 	// Rule 3.6.37
 	// Rule 3.6.38
